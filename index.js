35999c122d1bc169fcbf9c4c3362df1c8f54a503
pamratchadaphon <139990701+pamratchadaphon@users.noreply.github.com>
2024-07-29 23:14:21 +0700

    update port

--- index.js
+++ index.js
@@ -48,4 +48,8 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
+app.listen(8080, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 module.exports = app;
